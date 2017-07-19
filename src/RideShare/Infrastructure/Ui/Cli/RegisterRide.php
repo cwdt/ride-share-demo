2d044be1e79e04b48299549b395dd256265e33b4
Marco Veenendaal <marco@cwdt.nl>
2017-07-19 18:23:41 +0200

    Initial commit

---
+++ src/RideShare/Infrastructure/Ui/Cli/RegisterRide.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RideShare\Infrastructure\Ui\Cli;
+
+use Psr\Log\LoggerInterface;
+use RideShare\Application\RegisterRide\RegisterRideCommand;
+use SimpleBus\Message\Bus\MessageBus;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CliRegisterRideCommand extends Command
+{
+    /** @var MessageBus */
+    protected $commandBus;
+
+    /** @var LoggerInterface */
+    protected $logger;
+
+    /**
+     * @param MessageBus $commandBus
+     * @param LoggerInterface $logger
+     */
+    public function __construct(MessageBus $commandBus, LoggerInterface $logger)
+    {
+        $this->commandBus = $commandBus;
+        $this->logger = $logger;
+        parent::__construct(null);
+    }
+
+    /**
+     *
+     */
+    public function configure()
+    {
+        $this->setName('ride-share:ride:register');
+        $this->setDescription('Register a ride');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->commandBus->handle(
+            new RegisterRideCommand(
+                uniqid(),
+                10,
+                10,
+                10,
+                10,
+                '2017-10-10 10:00:00'
+            )
+        );
+    }
+}
